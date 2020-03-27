7d83c8053315501c53d724f4a25a4bbd53c81ece
AndersonGaspardin <48970507+AndersonGaspardin@users.noreply.github.com>
2020-03-27 02:45:21 -0300

    Update sprint02.py

--- sprint02.py
+++ sprint02.py
@@ -11,7 +11,8 @@ gc.collect()
 token = input("Informe seu token do github: ")
 headers = {"Authorization": "Bearer " + token} 
 
-def run_query(query, headers): # A simple function to use requests.post to make the API call. Note the json= section.
+#Funcao para , atraves da funcao requests.post da biblioteca requests, fazer uma chamada da API do graphql
+def run_query(query, headers): 
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
     if request.status_code == 200:
         return request.json()
@@ -20,7 +21,7 @@ def run_query(query, headers): # A simple function to use requests.post to make 
 
 query = """
 query lab2S2 {
-  search(query: "stars:>100", type: REPOSITORY, first: 50{AFTER}) {
+  search(query: "stars:>1", type: REPOSITORY, first: 100{AFTER}) {
     nodes {
       ... on Repository {
         forks {
@@ -54,6 +55,7 @@ query lab2S2 {
 
 """
 
+
 #substituindo o comando after na consulta inicial
 finalQuery = query.replace("{AFTER}", "")
 
@@ -65,6 +67,7 @@ nodes = resultado['data']['search']['nodes']
 
 conta_repositorios_python = 0
 
+total_pages =0 
 #preparando o cabecalho do arquivo csv
 with open("d:/LAB-Exp-Software/LAB02/repos-python.csv", 'w') as arquivo_repositorios_gvan:
 
@@ -72,28 +75,31 @@ with open("d:/LAB-Exp-Software/LAB02/repos-python.csv", 'w') as arquivo_reposito
   writer = DictWriter(arquivo_repositorios_gvan, fieldnames=cabecalho)
   writer.writeheader()
 
-  #paginando a query no graphql
+
   while (conta_repositorios_python < 1000):
       
-      for node in nodes:
-        #verificando a linguagem primaria do repositorio
-        linguagem_primaria = node['primaryLanguage']
-        #se linguagem primary (primaryLanguage) for igual a Python entao escreve no csv
-        if linguagem_primaria == {'name':'Python'}:
-          writer.writerow(node)
-          conta_repositorios_python += 1
-
       #montando as query apos a primeira, detectando o cursor da ultima entrada da query anterior
       # adicionando o campo after na query do graphql
       # rodando o script com a nova query 
       #carregando o json em um dicionario python
       cursor = resultado["data"]["search"]["pageInfo"]["endCursor"]
       next_query = query.replace("{AFTER}", ", after: \"%s\"" % cursor)
-      result = run_query(next_query, headers)
+      resultado = run_query(next_query, headers)
       nodes = resultado['data']['search']['nodes']
+      next_page  = resultado["data"]["search"]["pageInfo"]["hasNextPage"]
+      total_pages += 1
+
+      
+      for node in nodes:
+        
+        linguagem_primaria = node['primaryLanguage']
+        #se linguagem primary (primaryLanguage) for igual a Python entao escreve no csv
+        if linguagem_primaria == {'name':'Python'}:
+
+            writer.writerow(node)
+            conta_repositorios_python += 1
+            print(f"arquivos encontrados {node['name']} {conta_repositorios_python}")
+
+      
 
 print("Execucao encerrada!")
-
-
-
-
